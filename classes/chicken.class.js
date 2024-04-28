48db4a73f4dfa1619816530008924cd8f4c20432
Christina Bernhardt <sozia76@web.de>
2024-04-28 21:59:04 +0200

    constructor

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,6 +1,10 @@
 class Chicken extends MovableObject {
  
-
+    constructor(){
+        super().loadImage('assets/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+    
+    
+     }
 
 
 
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -3,6 +3,12 @@ class MovableObject {
     y;
     img;
 
+    // loadImage('img/test.png')
+    loadImage(path){
+        this.img = new Image(); //Abkürzung 
+        this.img.src = path;
+    }
+
     moveRight(){
         console.log('Moving right');
     }
