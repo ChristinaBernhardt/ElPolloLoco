15379d7a0f6569b73ee7d932ad5a48f6f7059c74
Christina Bernhardt <sozia76@web.de>
2024-05-03 23:44:05 +0200

    Pepe animated

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,5 +1,6 @@
 class Character extends MovableObject {
   x = 200;
+  speed = 10;
   IMAGES_WALKING = [
     "assets/2_character_pepe/2_walk/W-21.png",
     "assets/2_character_pepe/2_walk/W-22.png",
@@ -9,30 +10,24 @@ class Character extends MovableObject {
     "assets/2_character_pepe/2_walk/W-26.png",
   ];
 
+  world;
 
   constructor() {
     super().loadImage("assets/2_character_pepe/2_walk/W-21.png");
 
     this.loadImages(this.IMAGES_WALKING);
-  
+
     this.animate();
- 
   }
 
   animate() {
-
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
+        if (this.world.keyboard.RIGHT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+        }
     }, 100);
-
-this.moveLeft();
-   };
-
-
-
-
-
+  }
 }
--- js/game.js
+++ js/game.js
@@ -1,11 +1,58 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
-function init(){
-    canvas = document.getElementById('canvas');
-    world = new World(canvas);
+function init() {
+  canvas = document.getElementById('canvas');
+  world = new World(canvas, keyboard);
 
-
-
-console.log('My Character is', world.character);
+  console.log("My Character is", world.character);
 }
+
+// let space = false;
+// let left = false;
+// let RIGHT = false;
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = true;
+      }
+  if (e.keyCode == 37) {
+    keyboard.LEFT = true;
+  }
+  if (e.keyCode == 38) {
+    keyboard.UP = true;
+  }
+  if (e.keyCode == 40) {
+    keyboard.DOWN = true;
+  }
+  if (e.keyCode == 32) {
+    keyboard.SPACE = true;
+  }
+
+  console.log(e);
+
+  console.log("Hello", keyboard);
+});
+
+
+window.addEventListener("keyup", (e) => {
+    if (e.keyCode == 39) {
+      keyboard.RIGHT = false;
+    }
+    if (e.keyCode == 37) {
+      keyboard.LEFT = false;
+    }
+    if (e.keyCode == 38) {
+      keyboard.UP = false;
+    }
+    if (e.keyCode == 40) {
+      keyboard.DOWN = false;
+    }
+    if (e.keyCode == 32) {
+      keyboard.SPACE = false;
+    }
+  
+    console.log(e);
+  }
+);
