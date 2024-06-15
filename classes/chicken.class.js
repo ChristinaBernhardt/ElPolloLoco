bff1215360f9da2c5b0c743f0d5a8985ade030f5
Christina Bernhardt <sozia76@web.de>
2024-06-15 22:37:52 +0200

    finish

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -20,50 +20,50 @@ offset = {
 
   IMAGES_DEAD = ["assets/3_enemies_chicken/chicken_normal/2_dead/dead.png"];
 
-  /**
- * Creates an instance of Chicken.
+/**
+ * Represents a Chicken enemy in the game.
+ * Extends the Entity class and initializes with a walking animation and movement parameters.
  *
  * @constructor
  */
 constructor() {
-    super().loadImage("assets/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
-    this.loadImages(this.IMAGES_WALKING);
-    this.loadImages(this.IMAGES_DEAD);
-    this.x = 500 + Math.random() * 4000;
-    this.speed = 0.15 + Math.random() * 0.5;
-    this.animate();
-  }
+  super().loadImage("assets/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+  this.loadImages(this.IMAGES_WALKING);
+  this.loadImages(this.IMAGES_DEAD);
+  this.x = 500 + Math.random() * 4000;
+  this.speed = 0.15 + Math.random() * 0.5;
+  this.animate();
+}
 
-
-  /**
- * animate chicken
- * 
+/**
+ * Animates the chicken's movement and walking animation.
+ * Starts two intervals: one for continuous movement to the left and one for playing walking animation frames.
  */
 animate() {
-    this.intervalIdAnimate = setInterval(() => {
-      this.moveLeft();
-       if (this.x <= 0) {
-        clearInterval(this.interval); 
-      }
-    }, 1000 / 60); 
-    this.intervalIdPlayAnimation = setInterval(() => {
-      if (this.dead != true) {
-        this.playAnimation(this.IMAGES_WALKING);
-      }
-    }, 50);
-  }
+  this.intervalIdAnimate = setInterval(() => {
+    this.moveLeft();
+    if (this.x <= 0) {
+      clearInterval(this.intervalIdAnimate); // Stop moving when reaching the edge
+    }
+  }, 1000 / 60);
 
+  this.intervalIdPlayAnimation = setInterval(() => {
+    if (!this.dead) {
+      this.playAnimation(this.IMAGES_WALKING);
+    }
+  }, 50);
+}
 
-  /**
- * let chicken die
- * 
+/**
+ * Causes the chicken to die.
+ * Sets the 'dead' flag, changes the image to a dead chicken, and stops animation intervals after a delay.
  */
 die() {
-    this.dead = true;
-    this.loadImage(this.IMAGES_DEAD);
-    clearInterval(this.intervalIdAnimate);
-    setTimeout(() => {
-      clearInterval(this.intervalIdPlayAnimation);
-    }, 500);
-  }
+  this.dead = true;
+  this.loadImage(this.IMAGES_DEAD);
+  clearInterval(this.intervalIdAnimate);
+  setTimeout(() => {
+    clearInterval(this.intervalIdPlayAnimation);
+  }, 500);
+}
 }
--- classes/drawable-object.class.js
+++ classes/drawable-object.class.js
@@ -10,59 +10,57 @@ imageCache = {};
 currentImage = 0;
 
  
-  /**
- * load image
+ /**
+ * Loads an image from the given path.
  *
- * @param {*} path
+ * @param {string} path - The path to the image file.
  */
 loadImage(path) {
-    this.img = new Image(); 
-    this.img.src = path;
-  }
+  this.img = new Image();
+  this.img.src = path;
+}
 
-
-  /**
- * draw image
+/**
+ * Draws the loaded image onto the canvas context.
  *
- * @param {*} ctx
+ * @param {CanvasRenderingContext2D} ctx - The 2D rendering context of the canvas.
  */
 draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
+  ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+}
 
-
-  /**
- * load images
+/**
+ * Preloads images from an array of paths into the image cache.
  *
- * @param {*} arr
+ * @param {string[]} arr - Array of paths to the image files to preload.
  */
 loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
-  }
+  arr.forEach((path) => {
+    let img = new Image();
+    img.src = path;
+    this.imageCache[path] = img;
+  });
+}
 
-
-  /**
- * get image index for status bars
+/**
+ * Determines the image index based on the current percentage value.
+ * Useful for status bars or similar UI elements.
  *
- * @returns {(5 | 4 | 3 | 2 | 1 | 0)}
+ * @returns {5 | 4 | 3 | 2 | 1 | 0} - Image index based on the percentage value.
  */
 resolveImageIndex() {
-    if (this.percentage == 100) {
-      return 5;
-    } else if (this.percentage >= 80) {
-      return 4;
-    } else if (this.percentage >= 60) {
-      return 3;
-    } else if (this.percentage >= 40) {
-      return 2;
-    } else if (this.percentage >= 20) {
-      return 1;
-    } else {
-      return 0;
-    }
+  if (this.percentage == 100) {
+    return 5;
+  } else if (this.percentage >= 80) {
+    return 4;
+  } else if (this.percentage >= 60) {
+    return 3;
+  } else if (this.percentage >= 40) {
+    return 2;
+  } else if (this.percentage >= 20) {
+    return 1;
+  } else {
+    return 0;
   }
+}
 }
